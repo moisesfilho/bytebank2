712f16ff246822b5bb466fe949cd7a7988c6d20f
NOTE-MOISES\moisesfilho <moises.son@gmail.com>
2020-02-17 20:48:37 -0300

    iniciando adicionar contacts

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,22 @@ class BytebankApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Dashboard'),
         ),
-        body: Column(children: <Widget>[
-          Image.asset('images/bytebank_logo.png')
-        ],),
+        body: Column(
+          children: <Widget>[
+            Image.asset('images/bytebank_logo.png'),
+            Container(
+              height: 120,
+              width: 100,
+              color: Colors.green,
+              child: Column(
+                children: <Widget>[
+                  Icon(Icons.people),
+                  Text('Contacts')
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
