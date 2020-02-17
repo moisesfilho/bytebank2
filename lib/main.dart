cd8ed2613e61f4aa8acbaacee379cb4569e4bc4b
NOTE-MOISES\moisesfilho <moises.son@gmail.com>
2020-02-17 20:58:21 -0300

    mais definições de alinhamento

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,20 @@ class BytebankApp extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Image.asset('images/bytebank_logo.png'),
             ),
-            Container(
-              height: 120,
-              width: 100,
-              color: Colors.green,
-              child: Column(
-                children: <Widget>[
-                  Icon(Icons.people),
-                  Text('Contacts')
-                ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 150,
+                color: Colors.green,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(Icons.people),
+                    Text('Contacts')
+                  ],
+                ),
               ),
             )
           ],
