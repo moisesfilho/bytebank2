10611cce82f6508a8f5b924e7f2d455553fe6640
NOTE-MOISES\moisesfilho <moises.son@gmail.com>
2020-02-17 20:44:36 -0300

    Projeto inicial já com logo

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(children: <Widget>[
+          Image.asset('images/bytebank_logo.png')
+        ],),
+      ),
+    );
+  }
+}
