0585989b860a12d65d8ef6cb2871ef3c98b5f869
NOTE-MOISES\moisesfilho <moises.son@gmail.com>
2020-05-05 20:43:59 -0300

    Atualizando lista de contatos;

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,28 +1,22 @@
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contacts_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
+  final List<Contact> contacts = List();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Contacts'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Moisés',
-                style: TextStyle(fontSize: 24.0),
-              ),
-              subtitle: Text(
-                '1000',
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-          )
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final Contact contact = contacts[index];
+          return _ContactItem(contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -37,6 +31,28 @@ class ContactsList extends StatelessWidget {
               );
         },
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(fontSize: 16.0),
+        ),
       ),
     );
   }
