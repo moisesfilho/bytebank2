e70e7519d332cba35f4e7afbea5e9cb5884f57fe
NOTE-MOISES\moisesfilho <moises.son@gmail.com>
2020-06-13 14:59:38 -0300

    Melhorando log do interceptador http

--- lib/http/webClient.dart
+++ lib/http/webClient.dart
@@ -2,13 +2,16 @@ import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
 void findAll() async {
-  final Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
-  final Response response = await client.get('http://192.168.0.113:8080/transactions');
+  final Client client =
+      HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+  final Response response =
+      await client.get('http://192.168.0.113:8080/transactions');
 }
 
 class LoggingInterceptor implements InterceptorContract {
   @override
   Future<RequestData> interceptRequest({RequestData data}) async {
+    print('Request');
     print('baseUrl: ${data.baseUrl}');
     print('method: ${data.method}');
     print('headers: ${data.headers}');
@@ -18,8 +21,10 @@ class LoggingInterceptor implements InterceptorContract {
 
   @override
   Future<ResponseData> interceptResponse({ResponseData data}) async {
-      print(data);
-      return data;
+    print('Response');
+    print('statusCode: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
   }
-
 }
