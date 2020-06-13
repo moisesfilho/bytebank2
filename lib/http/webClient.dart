3af562707dc702fbe36eb78a291c2aed1060046a
NOTE-MOISES\moisesfilho <moises.son@gmail.com>
2020-06-13 14:57:25 -0300

    Adicionando interceptador para realizar log

--- lib/http/webClient.dart
+++ lib/http/webClient.dart
@@ -1,6 +1,25 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
 
 void findAll() async {
-  final Response response = await get('http://192.168.0.113:8080/transactions');
-  print(response.body);
+  final Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+  final Response response = await client.get('http://192.168.0.113:8080/transactions');
+}
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print('baseUrl: ${data.baseUrl}');
+    print('method: ${data.method}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+      print(data);
+      return data;
+  }
+
 }
