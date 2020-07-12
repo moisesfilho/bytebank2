f6114b24d9989be3f33901c2cfd66b883e370d10
NOTE-MOISES\moisesfilho <moises.son@gmail.com>
2020-07-12 14:44:58 -0300

    Separando web cliente de transaction

--- lib/http/webClient.dart
+++ lib/http/webClient.dart
@@ -1,7 +1,3 @@
-import 'dart:convert';
-
-import 'package:bytebank/models/contact.dart';
-import 'package:bytebank/models/transaction.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
@@ -10,55 +6,3 @@ import 'interceptors/logging_interceptor.dart';
 final Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
 
 const String baseUrl = 'http://192.168.25.113:8080/transactions';
-
-Future<List<Transaction>> findAll() async {
-  final Response response = await client.get(baseUrl).timeout(Duration(seconds: 5));
-  if (response.statusCode != 200) {
-    return null;
-  }
-  final List<dynamic> decodedJson = jsonDecode(response.body);
-  final List<Transaction> transactions = List();
-
-  for (Map<String, dynamic> transactionJson in decodedJson) {
-    final Map<String, dynamic> contactJson = transactionJson['contact'];
-    final Transaction transaction = Transaction(
-      transactionJson['value'],
-      Contact(
-        0,
-        contactJson['name'],
-        contactJson['accountNumber'],
-      ),
-    );
-    transactions.add(transaction);
-  }
-
-  return transactions;
-}
-
-Future<Transaction> save(Transaction transaction) async {
-  final Map<String, dynamic> transactionMap = {
-    'value': transaction.value,
-    'contact': {'name': transaction.contact.name, 'accountNumber': transaction.contact.accountNumber}
-  };
-
-  final String transactionJson = jsonEncode(transactionMap);
-
-  final Response response = await client.post(baseUrl,
-      headers: {
-        'Content-type': 'application/json',
-        'password': '1000',
-      },
-      body: transactionJson);
-
-  Map<String, dynamic> json = jsonDecode(response.body);
-
-  final Map<String, dynamic> contactJson = json['contact'];
-  return Transaction(
-    json['value'],
-    Contact(
-      0,
-      contactJson['name'],
-      contactJson['accountNumber'],
-    ),
-  );
-}
