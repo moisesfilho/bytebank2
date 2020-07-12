80ec85893d314e1c15e95cf44c400a8becc064a9
NOTE-MOISES\moisesfilho <moises.son@gmail.com>
2020-07-12 15:11:03 -0300

    Criando parses e refatorando para utilizar no webclient

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -3,7 +3,14 @@ class Contact {
   final String name;
   final int accountNumber;
 
-  Contact(this.id, this.name, this.accountNumber); 
+  Contact(this.id, this.name, this.accountNumber);
+
+  Contact.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        accountNumber = json['accountNumber'];
+
+  Map<String, dynamic> toJson() => {'id': id, 'name': name, 'accountNumber': accountNumber};
 
   @override
   String toString() => 'Contatc(id: $id, name: $name, accountNumber: $accountNumber)';
