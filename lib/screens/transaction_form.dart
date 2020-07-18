85d3e9b6290e54c5117caa6ce7c2ff77c358d6f7
NOTE-MOISES\moisesfilho <moises.son@gmail.com>
2020-07-18 15:32:36 -0300

    Tratando contexto isolado do dialog

--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -66,14 +66,10 @@ class _TransactionFormState extends State<TransactionForm> {
 
                       showDialog(
                         context: context,
-                        builder: (context) {
+                        builder: (contextDialog) {
                           return TransactionAuthDialog(
                             onConfirm: (String password) {
-                              _webClient.save(transactionCreated, password).then((transaction) {
-                                if (transaction != null) {
-                                  Navigator.pop(context);
-                                }
-                              });
+                              _save(transactionCreated, password, context);
                             },
                           );
                         },
@@ -87,5 +83,13 @@ class _TransactionFormState extends State<TransactionForm> {
         ),
       ),
     );
+  }
+
+  void _save(Transaction transactionCreated, String password, BuildContext context) async {
+    _webClient.save(transactionCreated, password).then((transaction) {
+      if (transaction != null) {
+        Navigator.pop(context);
+      }
+    });
   }
 }
