15995715ad74c580af849bd9cd2af30735b90255
NOTE-MOISES\moisesfilho <moises.son@gmail.com>
2020-07-18 16:12:28 -0300

    Refatorando save da transaction no webclient

--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -24,14 +24,17 @@ class TransactionWebClient {
         },
         body: transactionJson);
 
-    if (response.statusCode == 400) {
-      throw Exception('there was an error sumitting transaction');
+    if (response.statusCode == 200) {
+      return Transaction.fromJson(jsonDecode(response.body));
     }
 
-    if (response.statusCode == 401) {
-      throw Exception('authentication failed');
-    }
-
-    return Transaction.fromJson(jsonDecode(response.body));
+    _throwHttpError(response.statusCode);
   }
+
+  void _throwHttpError(int statusCode) => throw Exception(_statusCodeReponses[statusCode]);
+
+  static final Map<int, String> _statusCodeReponses = {
+    400: 'there was an error sumitting transaction',
+    401: 'authentication failed'
+  };
 }
