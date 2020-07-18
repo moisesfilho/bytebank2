914aa89219d7449e459c8ba9754b700cd9229ff0
NOTE-MOISES\moisesfilho <moises.son@gmail.com>
2020-07-18 16:01:42 -0300

    Adicionando dialogos de sucesso e falha

--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/components/response_dialog.dart';
 import 'package:bytebank/components/transaction_auth_dialog.dart';
 import 'package:bytebank/http/webclients/transaction_webclient.dart';
 import 'package:bytebank/models/contact.dart';
@@ -88,10 +89,19 @@ class _TransactionFormState extends State<TransactionForm> {
   void _save(Transaction transactionCreated, String password, BuildContext context) async {
     _webClient.save(transactionCreated, password).then((transaction) {
       if (transaction != null) {
-        Navigator.pop(context);
+        showDialog(
+            context: context,
+            builder: (contextDialog) {
+              return SuccessDialog('sucessful transaction');
+            }).then((value) => Navigator.pop(context));
       }
     }).catchError((e) {
-      print(e);
-    });
+      showDialog(
+        context: context,
+        builder: (contextDialog) {
+          return FailureDialog(e.message);
+        },
+      );
+    }, test: (e) => e is Exception);
   }
 }
