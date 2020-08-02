aed2cacf46b885c3b05dfbfd59d1c947547a2edb
moisesfilho <moises.son@gmail.com>
2020-08-02 17:31:39 -0300

    Teste buscando widgets especificos

--- test/dashboard_widget_test.dart
+++ test/dashboard_widget_test.dart
@@ -9,9 +9,25 @@ void main() {
     expect(mainImage, findsOneWidget);
   });
 
-  testWidgets('should display the first feature when the dashboard is opened', (tester) async {
+  testWidgets('should display greater than one feature when the dashboard is opened', (tester) async {
     await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final firstFeature = find.byType(FeatureItem);
-    expect(firstFeature, findsWidgets);
+    final anyFeature = find.byType(FeatureItem);
+    expect(anyFeature, findsWidgets);
+  });
+
+  testWidgets('should display the transfer feature when the dashboard is opened', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final iconTransferFeatureItem = find.widgetWithIcon(FeatureItem, Icons.monetization_on);
+    expect(iconTransferFeatureItem, findsOneWidget);
+    final nameTransferFeatureItem = find.widgetWithText(FeatureItem, 'Transfer');
+    expect(nameTransferFeatureItem, findsOneWidget);
+  });
+
+  testWidgets('should display the transaction feed feature when the dashboard is opened', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final iconTransactionFeedFeatureItem = find.widgetWithIcon(FeatureItem, Icons.description);
+    expect(iconTransactionFeedFeatureItem, findsOneWidget);
+    final nameTransactionFeedFeatureItem = find.widgetWithText(FeatureItem, 'Transaction Feed');
+    expect(nameTransactionFeedFeatureItem, findsOneWidget);
   });
 }
