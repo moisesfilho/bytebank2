f946d5ec36432f653303c12e0c163b3af06bb214
moisesfilho <moises.son@gmail.com>
2020-08-04 21:14:34 -0300

    Utilizando group nos testes

--- test/dashboard_widget_test.dart
+++ test/dashboard_widget_test.dart
@@ -3,45 +3,47 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('should display the main image when the dashboard is opened', (tester) async {
-    await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final mainImage = find.byType(Image);
-    expect(mainImage, findsOneWidget);
-  });
+  group('when dashboard is opened', () {
+    testWidgets('should display the main image', (tester) async {
+      await tester.pumpWidget(MaterialApp(home: Dashboard()));
+      final mainImage = find.byType(Image);
+      expect(mainImage, findsOneWidget);
+    });
 
-  testWidgets('should display greater than one feature when the dashboard is opened', (tester) async {
-    await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final anyFeature = find.byType(FeatureItem);
-    expect(anyFeature, findsWidgets);
-  });
+    testWidgets('should display greater than one feature', (tester) async {
+      await tester.pumpWidget(MaterialApp(home: Dashboard()));
+      final anyFeature = find.byType(FeatureItem);
+      expect(anyFeature, findsWidgets);
+    });
 
-  testWidgets('should display the transfer feature when the dashboard is opened', (tester) async {
-    await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final iconTransferFeatureItem = find.widgetWithIcon(FeatureItem, Icons.monetization_on);
-    expect(iconTransferFeatureItem, findsOneWidget);
-    final nameTransferFeatureItem = find.widgetWithText(FeatureItem, 'Transfer');
-    expect(nameTransferFeatureItem, findsOneWidget);
-  });
+    testWidgets('should display the transfer feature', (tester) async {
+      await tester.pumpWidget(MaterialApp(home: Dashboard()));
+      final iconTransferFeatureItem = find.widgetWithIcon(FeatureItem, Icons.monetization_on);
+      expect(iconTransferFeatureItem, findsOneWidget);
+      final nameTransferFeatureItem = find.widgetWithText(FeatureItem, 'Transfer');
+      expect(nameTransferFeatureItem, findsOneWidget);
+    });
 
-  testWidgets('should display the transaction feed feature when the dashboard is opened', (tester) async {
-    await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final iconTransactionFeedFeatureItem = find.widgetWithIcon(FeatureItem, Icons.description);
-    expect(iconTransactionFeedFeatureItem, findsOneWidget);
-    final nameTransactionFeedFeatureItem = find.widgetWithText(FeatureItem, 'Transaction Feed');
-    expect(nameTransactionFeedFeatureItem, findsOneWidget);
-  });
+    testWidgets('should display the transaction feed feature', (tester) async {
+      await tester.pumpWidget(MaterialApp(home: Dashboard()));
+      final iconTransactionFeedFeatureItem = find.widgetWithIcon(FeatureItem, Icons.description);
+      expect(iconTransactionFeedFeatureItem, findsOneWidget);
+      final nameTransactionFeedFeatureItem = find.widgetWithText(FeatureItem, 'Transaction Feed');
+      expect(nameTransactionFeedFeatureItem, findsOneWidget);
+    });
 
-  testWidgets('should display the transfer feature when the dashboard is opened', (tester) async {
-    await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final transferFeatureItem = find.byWidgetPredicate(
-        (widget) => widget is FeatureItem && widget.name == 'Transfer' && widget.icon == Icons.monetization_on);
-    expect(transferFeatureItem, findsOneWidget);
-  });
+    testWidgets('should display the transfer feature', (tester) async {
+      await tester.pumpWidget(MaterialApp(home: Dashboard()));
+      final transferFeatureItem = find.byWidgetPredicate(
+          (widget) => widget is FeatureItem && widget.name == 'Transfer' && widget.icon == Icons.monetization_on);
+      expect(transferFeatureItem, findsOneWidget);
+    });
 
-  testWidgets('should display the transaction feed feature when the dashboard is opened', (tester) async {
-    await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final transactionFeedFeatureItem = find.byWidgetPredicate(
-        (widget) => widget is FeatureItem && widget.name == 'Transaction Feed' && widget.icon == Icons.description);
-    expect(transactionFeedFeatureItem, findsOneWidget);
+    testWidgets('should display the transaction feed feature', (tester) async {
+      await tester.pumpWidget(MaterialApp(home: Dashboard()));
+      final transactionFeedFeatureItem = find.byWidgetPredicate(
+          (widget) => widget is FeatureItem && widget.name == 'Transaction Feed' && widget.icon == Icons.description);
+      expect(transactionFeedFeatureItem, findsOneWidget);
+    });
   });
 }
