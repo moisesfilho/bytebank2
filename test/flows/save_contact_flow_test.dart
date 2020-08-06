13e314d6765b3acfe8b8044937d8a9af212e8ae3
moisesfilho <moises.son@gmail.com>
2020-08-06 19:40:32 -0300

    Ajsutes de diretórios

--- test/flows/save_contact_flow_test.dart
+++ test/flows/save_contact_flow_test.dart
@@ -7,8 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-import 'matchers.dart';
-import 'mocks.dart';
+import '../matchers/matchers.dart';
+import '../mocks/mocks.dart';
 
 void main() {
   testWidgets('should save a contact', (tester) async {
